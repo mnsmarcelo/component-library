bbff237fa6248e2b6bc8d47788fb5228ab04964e
Marcelo Sousa <mns.marcelo67@gmail.com>
2023-07-29 20:59:45 -0300

    Change rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,24 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
-import typescript from "rollup-plugin-typescript2";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
-import { uglify } from "rollup-plugin-uglify";
-import { babel } from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
 
-export default {
-    input: "./stories/index.ts",
+export default [{
+  input: './stories/index.ts',
     output: [
         {
             file: "dist/index.js",
             format: 'es'
         }
     ],
-    plugins: [
-        commonjs(),
-        peerDepsExternal(),
-        resolve(),
-        typescript(),
-        postcss({
-            extensions: ['.css']
-        }),
-        uglify(),
-        babel()
-    ]
-}
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript(),
+    postcss({
+      extensions: ['.css']
+    })
+  ]
+}];
